e906675b0dcd472a493a3ccb8f227dae30a1c34f
JangYoHan <dygks5412@naver.com>
2022-05-18 16:24:10 +0900

    All Model in Mysql insert complete

--- restframeserver/restframeserver/urls.py
+++ restframeserver/restframeserver/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', views.users_list),
-    path('users/<int:pk>/', views.users),
+    path('users/<int:user_num>/', views.users),
     path('app_login/', views.app_login),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
